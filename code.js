2a99b08e8499e7d6d8cf5397a0a9057a57e7ff46
Suman Lama <sumnjlama@gmail.com>
2020-10-23 18:59:37 +0100

    draft commit

---
+++ code.js
@@ -0,0 +1,31 @@
+function hover(element) {
+    element.setAttribute('src', 'imgs/icons/git-icn-dark.svg')
+}
+
+function unhover(element) {
+    element.setAttribute('src', 'imgs/icons/behance icon 1.svg')
+}
+
+function openSite1() {
+    let openJhr = window.open("https://www.jhrnaturecure.com/");
+}
+function openSite2() {
+    let openSnowdonia = window.open("https://sumnjlama.github.io/projectSnowdon/index.html");
+}
+function openSite3() {
+    let openTripleBooster = window.open("https://www.behance.net/gallery/84703685/Triple-Booster-Design-project");
+}
+function openBehanceSite() {
+    let openTripleBooster = window.open("https://www.behance.net/sumanjlama");
+}
+function openGithubSite() {
+    let openTripleBooster = window.open("https://github.com/sumnjlama");
+}
+function openResume() {
+    let openTripleBooster = window.open("/files/Resume.pdf");
+}
+
+
+
+
+
